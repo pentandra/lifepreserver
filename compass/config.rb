9a1038ced3184946da0e7a431b20d4824b30399a
Chris Chapman <chris@theprocesstree.org>
2011-09-28 15:37:53 -0600

    Merged boilerplate and default layout

--- compass/config.rb
+++ compass/config.rb
@@ -1,8 +1,8 @@
 http_path    = "/" 
 project_path = "." 
-css_dir      = "output/assets/style" 
-sass_dir     = "content/assets/style" 
-images_dir   = "output/assets/images"
+css_dir      = "output/css" 
+sass_dir     = "content/css" 
+images_dir   = "output/images"
 
 # when using SCSS:
 sass_options = {
