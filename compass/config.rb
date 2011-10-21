decefd59eebd590296c48725e5eb1c1a969cea51
Chris Chapman <chris@pentandra.com>
2011-10-21 00:58:33 -0600

    Changed css to compressed

--- compass/config.rb
+++ compass/config.rb
@@ -15,7 +15,8 @@ sass_options = {
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :production) ? :compressed : :expanded
+# output_style = (environment == :production) ? :compressed : :expanded
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
