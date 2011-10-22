77000e32bb776f923c3a99fc80c647a926400d43
Chris Chapman <chris@pentandra.com>
2011-10-22 14:59:59 -0600

    Added Nanoc helpers

---
+++ lib/helpers.rb
@@ -0,0 +1,8 @@
+include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::Capturing
+include Nanoc3::Helpers::Filtering
+include Nanoc3::Helpers::HTMLEscape
+include Nanoc3::Helpers::LinkTo
+include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::Tagging
+include Nanoc3::Helpers::Text
