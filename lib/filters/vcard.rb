7ec335ec4a5bdd035166f160b7b2f26be5b31290
Chris Chapman <chris@pentandra.com>
2012-02-13 23:57:51 -0700

    Fixing a bunch of stuff

--- lib/filters/vcard.rb
+++ lib/filters/vcard.rb
@@ -7,15 +7,20 @@ class VcardFilter < Nanoc3::Filter
   def run(content, params = {})
 
     vcard = ""
-    puts @item[:teammembers].to_slug
     @item[:teammembers].each do |member|
       card = Vpim::Vcard::Maker.make2 do |maker|
 
         maker.add_name do |name|
-          name.given = member[:name]
+          name.given = member[:first_name]
+          name.family = member[:last_name]
         end
 
         maker.add_email(member[:email]) { |e| e.location = 'work' }
+
+        maker.add_tel(member[:phone]) do |tel|
+          tel.location = 'work'
+          tel.capability = 'voice'
+        end
 
       end
 
