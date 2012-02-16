9faab4e8b3cd3074997da558bcb6fdeac456497b
Chris Chapman <chris@pentandra.com>
2012-02-16 08:44:37 -0700

    Fixed erroneous command name

--- scripts/logoart.sh
+++ scripts/logoart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-jp2ajp2a --width=70 --invert --chars="   .+;/oqQ" ../static/images/logo.jpg >> ../static/humans.txt
+jp2a --width=70 --invert --chars="   .+;/oqQ" ../static/images/logo.jpg >> ../static/humans.txt
 
