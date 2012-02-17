5a1a1b2959c0f84b22ccd6e591f3884ad58e0de7
Chris Chapman <chris@pentandra.com>
2012-02-16 22:06:34 -0700

    Apparently copied over this file yesterday - checked out original

--- settings/local.rb
+++ settings/local.rb
@@ -1,8 +1,11 @@
-# Settings for the staging server
+# Settings for quickest compilation
 
 $include_drafts = true
-$use_minified_js = true
-$use_minified_css = true
+$recent_posts_limit = 4
+$use_minified_js = false
+$use_minified_css = false
 
 $add_google_analytics = false
-$add_livefyre_comments = false
+$add_disqus_comments = false
+$disqus_develop_mode = true
+$disqus_shortname = 'pentandra'
