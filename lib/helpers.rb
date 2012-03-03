725b485e0b2c3923d05202e57642d46d7d7b7d85
Chris Chapman <chris@pentandra.com>
2012-03-02 18:32:50 -0700

    Figured out how to include a new helper

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,3 +5,4 @@ include Nanoc::Helpers::HTMLEscape
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::Tagging
+include Nanoc::Helpers::Text
--- lib/helpers/team.rb
+++ lib/helpers/team.rb
@@ -1,13 +1,11 @@
-module Nanoc::Helpers
-  module Team
+module Team
 
-    def full_name(member)
-      member[:first_name] + " " + member[:last_name]
-    end
-
-    def image_path(member)
-      "/images/" + full_name(member).to_slug + "-150x150.jpg"
-    end
-
+  def full_name(member)
+    member[:first_name] + " " + member[:last_name]
   end
+
+  def image_path(member)
+    "/images/" + full_name(member).to_slug + "-150x150.jpg"
+  end
+
 end
