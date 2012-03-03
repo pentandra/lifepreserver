888dc6c2550b0667c8d632648e3f04f369824b7a
Chris Chapman <chris@pentandra.com>
2012-03-03 02:24:20 -0700

    Only rescue when the network is down.

--- lib/helpers/url_shortener.rb
+++ lib/helpers/url_shortener.rb
@@ -7,8 +7,8 @@ module UrlShortener
       googl = Shortly::Clients::Googl
       googl.apiKey = "AIzaSyBrWtN-MJPxlUaJy3hjpqv59P3_4w9BWho"
       googl.shorten(url).shortUrl
-    rescue
-      url # Just return the original if we have a problem
+    rescue SocketError
+      url # Just return the original if we have a network problem
     end
   end
 
