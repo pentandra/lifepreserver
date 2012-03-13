3b8d26e4a7285042d7983abf4a761a227b941f6f
Chris Chapman <chris@pentandra.com>
2012-03-12 23:08:52 -0600

    Added nanoc command to set the environment

---
+++ commands/set_env.rb
@@ -0,0 +1,22 @@
+usage 'set_env [options]'
+aliases :se
+summary 'set the current environment'
+
+option :e, :env, 'specify the environment', :argument => :required
+
+run do  |opts, args, cmd|
+
+  # get environment
+  environment = opts.fetch(:env) do
+    $stderr.puts "The set_env command requires an --env option"
+    exit 1
+  end
+
+  if !File.exists?("environments/#{environment}.rb")
+    $stderr.puts "Environment '#{environment}' does not exist."
+    exit 1
+  end
+
+  system('cp', "environments/#{environment}.rb", 'environment.rb')
+
+end
