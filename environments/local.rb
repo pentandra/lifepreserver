7f5b5148366a2869c3838426da8ffa01bf9e86c1
Chris Chapman <chris@pentandra.com>
2012-04-22 09:49:09 -0600

    Fixed article id so it is explicit

--- environments/local.rb
+++ environments/local.rb
@@ -7,6 +7,7 @@ $use_minified_js = false
 $use_minified_css = false
 
 $add_google_analytics = false
-$add_disqus_comments = false
-$disqus_develop_mode = true
-$disqus_shortname = 'pentandra'
+
+$add_comments = false
+$livefyre_domain = "pentandra.com"
+$livefyre_siteid = 291113
