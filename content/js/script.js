f73b183210ec0897a6054abfe56dae001380470d
Chris Chapman <chris@pentandra.com>
2012-09-17 23:32:55 -0600

    Scroll to anchor whenever the hash changes
    * Added a hashchange library
    * Moved code into modules for better encapsulation

--- content/js/script.js
+++ content/js/script.js
@@ -2,44 +2,104 @@
 
 */
 
+window.Pentandra = {};
+
+Pentandra.Kerning = (function() {
+
+  return {
+    init: function() {
+
+      $('#ourvalues h2').lettering();
+      $('.motto h1').lettering('words').children('span').lettering();
+      $('.teamintro h1').lettering();
+      $('#meetgeungle h2').lettering();
+
+    }
+  };
+
+}());
+
+Pentandra.Scrolling = (function() {
+
+  function setupScrollingFor(elem) {
+
+    $(elem).smoothScroll({
+      afterScroll: function(opts) {
+        location.hash = opts.scrollTarget;
+      }
+    });
+  
+  }
+
+  function scrollWhenTheHashChanges() {
+
+    $(window).hashchange(function() {
+      var hash = location.hash;
+
+      $.smoothScroll({
+        scrollTarget: hash
+      });
+    });
+
+  }
+
+  return {
+
+    init: function() {
+      setupScrollingFor('a');
+      scrollWhenTheHashChanges();
+
+      this.trigger();
+    },
+
+    trigger: function() {
+      $(window).hashchange();
+    }
+
+  };
+
+}());
+
+Pentandra.Social = (function() {
+
+  var opts = {
+
+    share: {
+      googlePlus: true,
+      twitter: true,
+      linkedin: true,
+      facebook: true
+    },
+
+    buttons: {
+      linkedin: { counter: 'right' },
+      twitter: { via: 'PentandraInc', related: 'geungle' }
+    },
+
+    urlCurl: '',
+
+    enableHover: false,
+    enableCounter: false,
+    enableTracking: true
+    
+  };
+
+  return {
+    init: function(target) {
+      if ($.fn.sharrre) {
+        $(target).sharrre(opts);
+      }
+    }
+  };
+
+}());
+
+
 $(document).ready(function() {
 
-  // Kerning
-  $('#ourvalues h2').lettering();
-  $('.motto h1').lettering('words').children('span').lettering();
-  $('.teamintro h1').lettering();
-  $('#meetgeungle h2').lettering();
-
-
-  $('a').smoothScroll({
-    afterScroll: function(opts) {
-      location.hash = opts.scrollTarget;
-    }
-  });
-  
-  if ($.fn.sharrre) {
-    $('#share').sharrre({
-
-      share: {
-        googlePlus: true,
-        twitter: true,
-        linkedin: true,
-        facebook: true
-      },
-
-      buttons: {
-        linkedin: { counter: 'right' },
-        twitter: { via: 'PentandraInc' }
-      },
-
-      urlCurl: '',
-
-      enableHover: false,
-      enableCounter: false,
-      enableTracking: true
-      
-    });
-  }
+  Pentandra.Kerning.init();
+  Pentandra.Scrolling.init();
+  Pentandra.Social.init('#share');
 
 });
 
