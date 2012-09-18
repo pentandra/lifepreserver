cd793b58eb8c9c8bc47aa17d8d82b2fd85e53c04
Chris Chapman <chris@pentandra.com>
2012-09-17 21:16:24 -0600

    Minify baselinejs

--- content/js/plugins.js
+++ content/js/plugins.js
@@ -45,46 +45,4 @@ window.log = function(){
 *
 * Date: Wed June 20 11:39:00 2012 GMT
 */
-(function( $ ) {
-
-	$.fn.baseline = function(breakpoints) {
-
-		// Set up our variables, like a good little developer
-		var tall, newHeight, base, old = 0;
-
-		return this.each(function(){
-			var $this = $(this); // Set the images as objects
-
-			var setbase = function(breakpoints) { // The fun starts here
-
-				// Check if a single value or multiple breakpoints are given                
-		                if (typeof breakpoints === 'number') {
-		                    base = breakpoints;
-		                } else if (typeof breakpoints === 'object') {
-		                    // Loop through the breakpoints and check which baseline to apply
-		                    for (key in breakpoints) {
-		                    	var current = parseInt(key);
-		                        if (document.width > current && current >= old) {
-		                            base = breakpoints[key];
-		                            old = current;
-		                        }
-		                    }
-		                }
-                
-				$this.css('maxHeight', 'none'); // Remove old max-height so that we can resize up as well as down
-				tall = $this.height(); // Grab the height
-				newHeight = Math.floor(tall / base) * base; // Make up a new height based on the baseline
-				$this.css('maxHeight', newHeight); // Set it!
-			}
-
-			setbase(breakpoints); // Call on load
-
-			$(window).resize(function(){ // And call again on resize.
-				setbase(breakpoints);
-			});
-
-		});
-
-	}
-
-}) ( jQuery );
+(function(e){e.fn.baseline=function(t){var n,r,i,s=0;return this.each(function(){var o=e(this),u=function(e){if(typeof e=="number")i=e;else if(typeof e=="object")for(key in e){var t=parseInt(key);document.width>t&&t>=s&&(i=e[key],s=t)}o.css("maxHeight","none"),n=o.height(),r=Math.floor(n/i)*i,o.css("maxHeight",r)};u(t),e(window).resize(function(){u(t)})})}})(jQuery);
