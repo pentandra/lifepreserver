48ef94beda4cd359dfa87955b3282d0a91d6ce72
Chris Chapman <chris@pentandra.com>
2014-12-16 00:04:05 -0700

    Add support for nicer ontologies in JSON-LD

--- lib/filters/rdfdistiller.rb
+++ lib/filters/rdfdistiller.rb
@@ -8,15 +8,16 @@ module Nanoc::Filters
 
     def run(content, params={})
 
+      in_format = params[:in] || "turtle"
+      out_format = params[:out] || "turtle"
+      base_uri = params[:base_uri] || @item[:base_uri] || @item.path
+
       options = {
         standard_prefixes: true,
         prefixes: {},
-        base_uri: params[:base_uri] || @item.path,
+        #base_uri: base_uri,
         validate: true,
       }
-
-      in_format = params[:in] || "turtle"
-      out_format = params[:out] || "turtle"
 
       graph = RDF::Repository.new
       
@@ -25,7 +26,37 @@ module Nanoc::Filters
 
       format = RDF::Format.for(out_format.to_sym).to_sym
 
-      graph.dump(format, options)
+      case format
+      when :jsonld
+        if @item[:context] then
+          context = JSON.parse(@item[:context])
+          options.merge!({ :context => context['@context'] })
+        end
+
+        jsonld = JSON.parse graph.dump(format, options)
+
+        base = jsonld['@graph'].select { |statement| statement['@id'] == base_uri }.first
+        unless base.nil? then
+          jsonld['@graph'].delete(base)
+          jsonld.merge!(base)
+        end
+
+        defines = jsonld['@graph'].select { |statement| statement.has_key?('rdfs:isDefinedBy') }
+        defines.each do |s|
+          jsonld['@graph'].delete(s)
+          s.delete('rdfs:isDefinedBy')
+        end
+        jsonld['defines'] = defines unless defines.empty?
+
+        jsonld.delete('@graph') if jsonld['@graph'].empty?
+
+        JSON.pretty_generate jsonld
+      when :rdfxml
+        graph.dump(format, options)
+      else
+        graph.dump(format, options)
+      end
+
     end
 
   end
