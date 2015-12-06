15a72657cf634db0d570cec51770965d694a0ec2
Chris Chapman <chris@pentandra.com>
2015-12-05 19:43:32 -0700

    Cache shortened uris

--- lib/helpers/url_shortener.rb
+++ lib/helpers/url_shortener.rb
@@ -1,17 +1,31 @@
-require "shortly"
+require 'shortly'
+require 'yaml'
 
 module LifePreserver
 
   module UrlShortener
 
+    include Nanoc::Helpers::Text
+
     def shorten(url)
-      begin
-        googl = Shortly::Clients::Googl
-        googl.apiKey = "AIzaSyBrWtN-MJPxlUaJy3hjpqv59P3_4w9BWho"
-        googl.shorten(url).shortUrl
-      rescue SocketError
-        url # Just return the original if we have a network problem
+
+      hash = md5(url)
+      short_urls = YAML.load_file('etc/short_urls.yaml')
+
+      short_url = short_urls[hash]
+
+      if not short_url then
+        begin
+          googl = Shortly::Clients::Googl
+          googl.apiKey = "AIzaSyBrWtN-MJPxlUaJy3hjpqv59P3_4w9BWho"
+          short_url = short_urls.store(hash, googl.shorten(url).shortUrl)
+          File.open('etc/short_urls.yaml', 'w+') { |io| io.write(YAML.dump(short_urls)) }
+        rescue SocketError
+          short_url = url # Just return the original if we have a network problem
+        end
       end
+
+      short_url
     end
 
   end
