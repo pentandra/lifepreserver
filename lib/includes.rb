ffa7da09375a463cbd03cbe286e0239a4a59bfa9
Chris Chapman <chris@pentandra.com>
2016-07-15 23:24:05 -0600

    Use ERB::Util instead of nanoc's HTMLEscape

--- lib/includes.rb
+++ lib/includes.rb
@@ -1,13 +1,13 @@
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::Capturing
 include Nanoc::Helpers::Filtering
-include Nanoc::Helpers::HTMLEscape
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::Tagging
 include Nanoc::Helpers::Text
 include Nanoc::Helpers::XMLSitemap
 include Nanoc::Helpers::CacheBusting
+include ERB::Util
 
 include Company
 include UrlShortener
