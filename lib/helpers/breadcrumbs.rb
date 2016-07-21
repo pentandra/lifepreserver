1814e9c76997eb5458db17169b4a0758369c9cda
Chris Chapman <chris@pentandra.com>
2016-07-21 17:40:40 -0600

    Reduce instead of map to excude non-existing items

--- lib/helpers/breadcrumbs.rb
+++ lib/helpers/breadcrumbs.rb
@@ -4,7 +4,11 @@ module Breadcrumbs
     components = @item.identifier.components
     identifiers = (0..components.length - 1).map { |len| len == 0 ? '/index.*' : "/#{components.first(len).join('/')}/index.*" }
 
-    identifiers.map { |id| @items[id] }
+    identifiers.reduce([]) do |memo, id|
+      found = @items[id]
+      memo << found if found
+    end
+
   end
 
 end
