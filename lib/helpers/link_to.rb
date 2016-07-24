e3d8de693640312af2869543f0ba58e58bc7d9bb
Chris Chapman <chris@pentandra.com>
2016-07-24 00:54:43 -0600

    Simplifying code

--- lib/helpers/link_to.rb
+++ lib/helpers/link_to.rb
@@ -15,24 +15,15 @@ module Nanoc::Helpers
     # @return [String] The absolute path to the target
     def absolute_path_to(item, target)
 
-      if @config[:base_url].nil?
-        raise Nanoc::Errors::GenericTrivial.new('Cannot build absolute path: site configuration has no base_url')
+      validate_config
+
+      path = target.is_a?(String) ? target : target.path
+
+      if path.nil?
+        raise "Cannot get the absolute path to #{target.inspect} because this target is not outputted (its routing rule returns nil)"
       end
 
-      # Find path
-      if target.is_a?(String)
-        path = target
-      else
-        path = target.path
-        if path.nil?
-          raise "Cannot get the absolute path to #{target.inspect} because this target is not outputted (its routing rule returns nil)"
-        end
-      end
-
-      absolute_path = unstack(item.path, path)
-      absolute_path = @config[:base_url] + absolute_path
-
-      absolute_path
+      @config[:base_url] + unstack(item.path, path)
     end
 
     def absolutify(item, content)
@@ -55,6 +46,12 @@ module Nanoc::Helpers
       # Remove relative paths
       p = p.gsub(/[^\/]*\/\.\.\//, '') while (p =~ /\.\.\//)
       p
+    end
+
+    def validate_config
+      if @config[:base_url].nil?
+        raise Nanoc::Errors::GenericTrivial.new('Cannot build absolute path: site configuration has no base_url')
+      end
     end
 
   end
