d60b6a38ed8708589522afe2aa383b08870591b6
Chris Chapman <chris@pentandra.com>
2016-08-01 15:32:55 -0600

    Add separate file for simple utils that are more low level

--- lib/default.rb
+++ lib/default.rb
@@ -1,15 +1,4 @@
 require 'html5small/nanoc'
 require 'nanoc/cachebuster'
 
-# Standalone method from https://gist.github.com/Integralist/9503099
-def symbolize(obj)
-  return obj.reduce({}) do |memo, (k, v)|
-    memo.tap { |m| m[k.to_sym] = symbolize(v) }
-  end if obj.is_a? Hash
-
-  return obj.reduce([]) do |memo, v| 
-    memo << symbolize(v); memo
-  end if obj.is_a? Array
-
-  obj
-end
+require_relative 'util'
