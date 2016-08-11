edf50e6c0524fa8e0fede6930d284e52a5d6c3eb
Chris Chapman <chris@pentandra.com>
2016-08-10 22:29:35 -0600

    Abstract away the template from the filter, so it can be managed in the nanoc rules

--- lib/filters/rdfdistiller.rb
+++ lib/filters/rdfdistiller.rb
@@ -6,15 +6,37 @@ module Nanoc::Filters
 
     identifiers :rdf_distiller
 
-    def run(content, params={})
+    # Distill the item into different
+    # [RDF](https://www.w3.org/TR/rdf11-concepts/) representations.
+    #
+    # This filter can be run as a filter and as a layout. This idea is borrowed
+    # from the Nanoc::Filters::XSL filter. The HTML/RDFa output format can only
+    # be run as a layout, since a template is needed by the RDF::Vocab
+    # extension `:to_html`.
+    #
+    # @example Invoking the filter as a layout
+    #
+    #     compile '/spec/**/*.ttl' do
+    #       layout '/template.*'
+    #     end
+    #
+    #     layout '/template.*', :rdf_distiller, output: :html
+    #
+    # @param [String] _content Ignored. As the filter can be run as a layout,
+    # the value of the `:content` parameter passed to the class at
+    # initialization is used as the content to distill.
+    #
+    # @param [Hash] params
+    #
+    # @return [String] The distilled content
+    def run(_content, params={})
 
-      input = params.fetch(:in, @item.identifier.ext)
-      output = params.fetch(:out, "turtle")
+      input = params.fetch(:input, @item.identifier.ext)
+      output = params.fetch(:output, "turtle")
 
       base_uri = params[:base_uri] || @item[:base_uri] || @config[:base_url] + @item.path
       prefix = params[:prefix] || @item[:prefix] || camelize(File.basename(@item.identifier.without_exts))
       prefixes = params.fetch(:prefixes, {})
-      template = params[:template] || File.expand_path("../../../etc/vocabulary.haml", __FILE__)
 
       output_format = RDF::Format.for(output.to_sym).to_sym
 
@@ -26,7 +48,7 @@ module Nanoc::Filters
 
       repository = RDF::Repository.new
       
-      RDF::Reader.for(input.to_sym).new(content, options) { |reader| repository << reader }
+      RDF::Reader.for(input.to_sym).new(assigns[:content], options) { |reader| repository << reader }
       
       if repository.query(predicate: RDF.type).map(&:object).include?(RDF::OWL.Ontology)
         vocab = RDF::Vocabulary.find(base_uri) || RDF::Vocabulary.from_graph(repository, url: base_uri, class_name: prefix.to_s.upcase)
@@ -37,7 +59,8 @@ module Nanoc::Filters
         when :jsonld 
           vocab.to_jsonld(graph: repository, prefixes: prefixes)
         when :rdfa
-          vocab.to_html(graph: repository, prefixes: prefixes, template: template)
+          raise 'The HTML/RDFa output format can only be run as a layout' unless assigns[:layout]
+          vocab.to_html(graph: repository, prefixes: prefixes, template: assigns[:layout][:filename])
         else
           repository.dump(output_format, options)
         end
