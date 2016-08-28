0c25af58034b3b23de0d68b7b8608d06a8cb3b57
Chris Chapman <chris@pentandra.com>
2016-08-28 00:55:03 -0600

    Add datasource for company and people info

--- lib/filters/vcard.rb
+++ lib/filters/vcard.rb
@@ -9,17 +9,22 @@ class VcardFilter < Nanoc::Filter
     validate params
 
     # Extract params
-    full_name = params[:full_name]
-    kind = params[:kind] || 'individual'
-    first_name = params[:first_name]
-    last_name = params[:last_name]
-    nick_name = params[:nick_name]
-    email = params[:email]
-    tel = params.key?(:extension) ? "#{params[:phone]};ext=#{params[:extension]}" : params[:phone]
-    url = params[:url] || @config[:base_url]
-    org = params[:org]
-    photo_uri = params[:photo_uri]
-    logo_uri = params[:logo_uri]
+    full_name = params[:full_name] || @item[:full_name] || "#{@item[:first_name]} #{@item[:last_name]}"
+    kind = params[:kind] || @item[:kind] || 'individual'
+    first_name = params[:first_name] || @item[:first_name]
+    last_name = params[:last_name] || @item[:last_name]
+    nick_name = params[:nick_name] || @item[:nick_name]
+
+    email = params[:email] || @item[:email]
+
+    ext = params[:extension] || @item[:extension]
+    tel = params[:phone] || @item[:phone]
+    tel = "#{tel};ext=#{ext}" if ext
+
+    url = params[:url] || @item[:url] || @config[:base_url]
+    org = params[:org] || @item[:org]
+    photo_uri = params[:photo_uri] || @item[:photo_uri]
+    logo_uri = params[:logo_uri] || @item[:logo_uri]
 
     vcard = VCardigan.create(version: '3.0')
 
@@ -63,8 +68,8 @@ class VcardFilter < Nanoc::Filter
   end
 
   def validate_required_parameters(params)
-    if params[:full_name].nil?
-      raise Nanoc::Errors::GenericTrivial.new('Cannot build vCard: the full_name parameter MUST be present')
+    if params[:full_name].nil? && @item[:full_name].nil? && "#{@item[:first_name]} #{@item[:last_name]}".empty?
+      raise Nanoc::Errors::GenericTrivial.new('Cannot build vCard: the full_name parameter MUST be present in either the params hash or item rep metadata')
     end
   end
 
--- lib/helpers/company.rb
+++ lib/helpers/company.rb
@@ -9,35 +9,46 @@ module Company
   end
 
   def member_path(member)
-    "#{company[:page_url]}##{full_name(member).to_slug}"
+    "#{@config[:company][:page_url]}##{full_name(member).to_slug}"
   end
 
   def member_description_path(member)
-    "#{company[:page_url]}/#sec:#{full_name(member).to_slug}"
+    "#{@config[:company][:page_url]}/#sec:#{full_name(member).to_slug}"
   end
 
-  def member_image_path(member)
+  def member_photo_url(member)
     "#{@config[:site][:images_url]}/#{full_name(member).to_slug}-150x150.jpg"
   end
 
+  # Fetch all the attributes of the company item.
+  #
+  # @return [Hash]
   def company
-    @config[:company]
+    @items['/company/_'].attributes
   end
 
-  def teammember(author_name)
-    teammembers = company[:teammembers]
-    teammembers.find { |member| full_name(member) == author_name }
+  def sorted_people
+    people = @items.find_all('/company/people/*')
+    people.sort_by do |p|
+      [ p[:last_name], p[:first_name] ]
+    end
   end
 
+  def person_by_name(full_name)
+    people = @items.find_all('/company/people/*')
+    people.find { |person| full_name(person) == full_name }
+  end
+
+  # run during preprocessing
   def generate_author_uris(item_set)
     item_set = @items if item_set.nil?
 
     validate_config
 
     item_set.each do |item|
-      teammember = teammember(item[:author_name]) if item.key?(:author_name)
-      if teammember then
-        item[:author_uri] ||= "#{@config[:base_url]}#{member_description_path(teammember)}"
+      author = person_by_name(item[:author_name])
+      if author
+        item[:author_uri] ||= "#{@config[:base_url]}#{member_description_path(author)}"
       end
     end
   end
