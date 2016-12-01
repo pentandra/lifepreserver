1a80038494e343e665ca4888821907d8820b2932
Chris Chapman <chris@pentandra.com>
2016-11-30 18:57:29 -0700

    An evolving implementation

--- lib/data_sources/dictionaries.rb
+++ lib/data_sources/dictionaries.rb
@@ -7,44 +7,47 @@ Class.new(Nanoc::DataSource) do
     Dir["#{@config.fetch(:dictionaries_dir)}/**/*.{yaml,dic}"].each do |dic|
       dic_filename = File.expand_path(dic)
       dic_name = File.basename(dic)
-      dic_lang = Dictionary.find_hunspell_lang(Pathname.new(dic).parent.basename)
-      raw_content = File.read(dic)
-      kind = case raw_content.lines.first
-             when /^[\d]+$/
-               'extra-dictionary'
+      dic_lang = SpellChecker::Dictionary.find_hunspell_lang(Pathname.new(dic).parent.basename)
+      first_line = File.open(dic, &:readline)
+      kind = case first_line
+             when /^(?<word_count>\d+)$/
+               # FIXME is there a better way to distinguish these?
+               $LAST_MATCH_INFO[:word_count].to_i > 10_000 ? 'base-dictionary' : 'extra-dictionary'
              when /^-{3,5}$/
                'acronym-dictionary'
              else
                'personal-dictionary'
              end
 
-      binary = (kind == 'extra-dictionary')
+      binary = %w(base-dictionary extra-dictionary).include?(kind)
 
-      attributes = {
-        name: dic_name,
-        kind: kind,
-        is_hidden: true,
-      }
-
+      raw_content = nil
       entries = case kind
                 when 'personal-dictionary'
+                  raw_content = File.read(dic)
                   lines = raw_content.lines.map(&:chomp)
                   { dic_entries: lines }
                 when 'acronym-dictionary'
+                  raw_content = File.read(dic)
                   dic_hash = YAML.load(raw_content)
                   { acronym_mappings: dic_hash, dic_entries: dic_hash.keys }
                 else
                   {}
                 end
 
-      attributes.merge!(entries)
+      attributes = {
+        name: dic_name,
+        kind: kind,
+        lang: dic_lang,
+        is_hidden: true,
+      }.merge(entries)
 
       items << new_item(
         binary ? dic_filename : Nanoc::Int::TextualContent.new(raw_content, filename: dic_filename),
         attributes,
         Nanoc::Identifier.new("/dictionaries/#{dic_lang}/#{dic_name}"),
         binary: binary,
-        checksum_data: "content=#{raw_content}",
+        checksum_data: binary ? "word_count=#{first_line}" : "content=#{raw_content}",
       )
     end
 
