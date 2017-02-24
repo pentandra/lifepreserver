348d6c512cef95d4072e2d29d14d3932a8d446ac
Chris Chapman <chris@pentandra.com>
2017-02-23 23:02:28 -0700

    Added to_sentence helper, wrapping activesupport

--- lib/helpers/text.rb
+++ lib/helpers/text.rb
@@ -1,10 +1,15 @@
 require 'active_support/core_ext/string/inflections'
+require 'active_support/core_ext/array/conversions'
 require 'digest/md5'
 
 module LifePreserver
   module Text
     def to_slug(separator = '-')
       self.to_s.parameterize(separator: separator)
+    end
+
+    def to_sentence(options = {})
+      Array(self).to_sentence(options)
     end
 
     def md5(text)
