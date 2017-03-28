6bae2caf788707d8b096c0a20808e647b75044b3
Chris Chapman <chris@pentandra.com>
2017-03-28 08:10:31 -0600

    Add rules for solutions

--- rules/solutions.rb
+++ rules/solutions.rb
@@ -1,3 +1,5 @@
+# Solutions
+
 compile '/static/solutions/proposals/*/index\.md' do
   filter :erb, @config[:erb]
   filter :pandoc, args: [
@@ -14,4 +16,23 @@ compile '/static/solutions/proposals/*/index\.md' do
   filter :rubypantsunicode
   filter :cache_buster if @config[:production]
   filter :html5small if @config[:production]
+  write item.identifier.without_ext.sub('/proposals', '') + '.html'
+end
+
+#compile '/static/solutions/proposals/*/index\.md', rep: :pdf do
+  #filter :erb
+  #filter :pandoc, args: [
+    #{ from: :markdown, to: :context },
+    #:chapters,
+    #:normalize,
+    #:smart,
+  #]
+  #filter :absolutify_paths, type: :context
+  #layout '/benefit_reports/report.*'
+  #filter :context2pdf, @config.fetch(:context2pdf, {}).merge(mode: @item.key?(:wip) ? 'draft' : 'publish')
+  #write item.identifier.without_ext.tr('-', '_') + '.pdf'
+#end
+
+route %r{/static/solutions(/(proposals|specifications))/.*} do |solution_type, _|
+  item.identifier.to_s.sub(solution_type, '')
 end
