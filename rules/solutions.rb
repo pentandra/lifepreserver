47503346222fa63bdc418e50c8efdf29dc11228e
Chris Chapman <chris@pentandra.com>
2017-03-28 10:07:56 -0600

    Move specifications rules to the solutions rules file

--- rules/solutions.rb
+++ rules/solutions.rb
@@ -33,6 +33,33 @@ end
   #write item.identifier.without_ext.tr('-', '_') + '.pdf'
 #end
 
+# Specification documents
+
+passthrough '/static/solutions/specifications/**/*.html'
+
+layout '/specifications/vocabulary.*', :rdf_distiller, format: :html
+
+compile '/static/solutions/specifications/**/*.ttl' do
+  filter :rdf_distiller, format: :turtle
+end
+
+compile '/static/solutions/specifications/**/*.ttl', rep: :jsonld do
+  filter :rdf_distiller, format: :jsonld
+  write item.identifier.without_ext + '.jsonld'
+end
+
+compile '/static/solutions/specifications/**/*.ttl', rep: :rdfxml do
+  filter :rdf_distiller, format: :rdfxml
+  write item.identifier.without_ext + '.rdf'
+end
+
+compile '/static/solutions/specifications/**/*.ttl', rep: :html do
+  layout '/specifications/vocabulary.*'
+  filter :cache_buster if @config[:production]
+  filter :html5small if @config[:production]
+  write item.identifier.without_ext + '.html'
+end
+
 route %r{/static/solutions(/(proposals|specifications))/.*} do |solution_type, _|
   item.identifier.to_s.sub(solution_type, '')
 end
