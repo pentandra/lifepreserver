65067d2cc66779f4b829cc63a8b1ef62076e71e0
Chris Chapman <chris@pentandra.com>
2017-09-14 17:29:35 -0600

    Filter the tag descriptions with the osf filter
    - Also had to put in a hack for the weird encoding bug (#77)

--- lib/filters/old_style_figures.rb
+++ lib/filters/old_style_figures.rb
@@ -53,7 +53,7 @@ class OldStyleFigures < Nanoc::Filter
       # if detects the `html` element.
       content = content.sub(%r{(<html[^>]+)xmlns="http://www.w3.org/1999/xhtml"}, '\1')
     else
-      content
+      content.dup.force_encoding(Encoding::UTF_8)
     end
   end
 
