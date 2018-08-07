1394ac72471d2b40a4e184a7bfeb32899179e098
Chris Chapman <chris@pentandra.com>
2018-08-06 23:50:50 -0600

    Add needed require statements

--- lib/data_sources/dependencies.rb
+++ lib/data_sources/dependencies.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'active_support/core_ext/string/inflections'
+require 'rubygems'
+require 'bundler'
 
 Class.new(Nanoc::DataSource) do
   identifier :dependencies
