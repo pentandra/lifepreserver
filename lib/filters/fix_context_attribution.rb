eb05cd3379fa43c0d13e98c064cb19c026674f4e
Chris Chapman <chris@pentandra.com>
2019-08-14 15:20:26 -0600

    Only match "---" at the beginning of the line

--- lib/filters/fix_context_attribution.rb
+++ lib/filters/fix_context_attribution.rb
@@ -1,3 +1,3 @@
 Nanoc::Filter.define(:fix_context_attribution) do |content, params|
-  content.gsub(/(---.+?\\par)/m, "\\startattribution\n\\1\n\\stopattribution\n")
+  content.gsub(/^(---.+?\\par)/m, "\\startattribution\n\\1\n\\stopattribution\n")
 end
