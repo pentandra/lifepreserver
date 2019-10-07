d4fad28dd7db110fc603ac9bd8aedb0abb01bd20
Chris Chapman <chris@pentandra.com>
2019-10-07 09:39:49 -0600

    Fix qrcode color issue and link to ChunkyPNG#Color documentation

--- lib/filters/qrcode.rb
+++ lib/filters/qrcode.rb
@@ -11,17 +11,22 @@ class QRCode < Nanoc::Filter
   # @param [String] content the content to encode
   # @param [Hash] params the options for the qrcode
   #
-  # @options params [nil] :version to autoselect the smallest version of the qrcode
-  #   based on the length of the content
-  # @options params [Integer] :version the specific version of the qrcode desired
+  # @options params [nil] :version To autoselect the smallest version of the qrcode
+  #   based on the length of the content.
+  # @options params [Integer] :version To manually select a specific version of the
+  #   qrcode.
   # @see https://www.qrcode.com/en/about/version.html
   #
   # @options params [:l, :m, :q, :h] :level (:h) the error correction level
   # @see https://www.qrcode.com/en/about/error_correction.html
   #
-  # @options params [Integer] :image_size (90) the desired output image size in pixels
-  # @options params [String] :color the foreground color
-  # @options params [String] :fill the background color
+  # @options params [Integer] :image_size (90) The desired output image size in pixels.
+  # @options params [Integer, String] :color ('black') The foreground color to use.
+  # @options params [Integer, String] :fill ('white') The background color to use.
+  # @options params [Integer] :color_mode The color mode to use. Use one of the
+  #   ChunkyPNG::COLOR_* constants.
+  # @see {ChunkyPNG#Color}
+  # @options params [Boolean] :interlace (false) Whether to use interlacing.
   def run(content, params = {})
     version = params[:version]
     level = params.fetch(:level, :h)
@@ -30,12 +35,14 @@ class QRCode < Nanoc::Filter
     image_size = params.fetch(:image_size, 90)
     color = params.fetch(:color, ChunkyPNG::Color::BLACK)
     fill = params.fetch(:fill, ChunkyPNG::Color::WHITE)
+    interlace = params.fetch(:interlace, false)
 
     qr = RQRCode::QRCode.new(content, size: version, level: level)
     qr.as_png(color: color,
               fill: fill,
               size: image_size,
               color_mode: ChunkyPNG::COLOR_INDEXED,
+              interlace: interlace,
               file: output_filename)
   end
 end
