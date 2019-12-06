f410306030bae629b8b6407d40216e88b3addd3f
Chris Chapman <chris@pentandra.com>
2019-12-05 17:39:42 -0700

    Refactored dictionary data source.

--- lib/data_sources/dictionaries.rb
+++ lib/data_sources/dictionaries.rb
@@ -19,8 +19,8 @@ module LifePreserver
         available_dictionaries = FFI::Hunspell.directories.map { |d| File.basename(d) }
         Locale.set_app_language_tags(*available_dictionaries)
 
-        # Set currently supported languages, in order of priority
-        Locale.set_current(*@config.fetch(:supported_locales, [Locale.default]))
+        # Set languages order of priority
+        Locale.set_current(*@config.fetch(:ordered_locales, [Locale.default]))
       end
 
       def items
@@ -37,50 +37,68 @@ module LifePreserver
         items
       end
 
-      protected
+      def dic_to_item(dic_path)
+        expanded_dic_path = File.expand_path(dic_path)
+        dirname, basename = File.split(expanded_dic_path)
+        parent_name = File.basename(dirname)
 
-      def dic_to_item(dic_filename)
-        first_line = File.open(dic_filename, &:readline)
-        kind = case first_line
-               when /^\d+$/
-                 # a base dictionary will have a similarly named affix file
-                 File.exist?(dic_filename.sub(/\.dic$/, '.aff')) ? 'base-dictionary' : 'extra-dictionary'
-               when /^-{3,5}$/
-                 'acronym-dictionary'
-               else
-                 'personal-dictionary'
-               end
+        kind = dictionary_peek(expanded_dic_path)
+
+        attributes = {
+          name: File.basename(dic_path, '.*'),
+          kind: kind,
+          lang: parent_name,
+          mtime: File.mtime(expanded_dic_path),
+          is_hidden: true,
+        }
+
+        extended_attributes =
+          case kind
+          when 'personal-dictionary'
+            { entries: File.readlines(expanded_dic_path, chomp: true) }
+          when 'acronym-dictionary'
+            acronym_dic = File.open(expanded_dic_path) do |dic|
+              YAML.safe_load(dic.read, filename: dic.path)
+            end
+            { acronym_mappings: acronym_dic, entries: acronym_dic.keys }
+          else
+            {}
+          end
 
         binary = %w[base-dictionary extra-dictionary].include?(kind)
 
-        entries = case kind
-                  when 'personal-dictionary'
-                    raw_content = File.read(dic_filename)
-                    lines = raw_content.lines.map(&:chomp)
-                    { entries: lines }
-                  when 'acronym-dictionary'
-                    raw_content = File.read(dic_filename)
-                    entries_hash = YAML.safe_load(raw_content)
-                    { acronym_mappings: entries_hash, entries: entries_hash.keys }
-                  else
-                    {}
-                  end
-
-        attributes = {
-          name: File.basename(dic_filename, '.*'),
-          kind: kind,
-          mtime: File.mtime(dic_filename),
-          is_hidden: true,
-        }.merge(entries)
-
-        filename = File.expand_path(dic_filename)
-        parent_name = File.basename(File.dirname(dic_filename))
         new_item(
-          binary ? filename : Nanoc::Core::TextualContent.new(raw_content, filename: filename),
-          attributes,
-          Nanoc::Identifier.new("/#{parent_name}/#{File.basename(filename)}"),
+          expanded_dic_path,
+          attributes.merge(extended_attributes),
+          File.join(File::SEPARATOR, parent_name, basename),
           binary: binary,
         )
+      end
+
+      # Discover the kind of dictionary by peeking at the first line of the
+      # given dictionary file.
+      #
+      # @see file:var/dictionaries/README.md
+      #
+      # @param dic_path [String] The path to the dictionary file.
+      #
+      # @return [String] The kind of dictionary, either a +base-dictionary+,
+      #   an +extra-dictionary+, an +acronym-dictionary+, or a
+      #   +personal-dictionary+.
+      def dictionary_peek(dic_path)
+        case File.open(dic_path, &:readline)
+        when /^\d+$/
+          # a base dictionary will have a similarly named affix file
+          if File.exist?(dic_path.sub(/\.dic$/, '.aff'))
+            'base-dictionary'
+          else
+            'extra-dictionary'
+          end
+        when /^-{3,5}$/
+          'acronym-dictionary'
+        else
+          'personal-dictionary'
+        end
       end
     end
   end
